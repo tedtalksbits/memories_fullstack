8b5b9ccf990a40a069b9de4aede26ba7561767e2
tedtalksbits <tedaneblake@gmail.com>
2020-12-31 23:16:48 -0500

    tweaks

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -5,7 +5,6 @@ const post = (posts = [], action) => {
       case DELETE:
          return posts.filter((post) => post._id !== action.payload);
       case UPDATE:
-      case 'LIKE':
          return posts.map((post) => post._id === action.payload._id ? action.payload : post)
       case FETCH_ALL:
          return action.payload;
