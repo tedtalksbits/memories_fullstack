e407c71f3edba90db83f7cce96bc2cae12d5e9e5
tedtalksbits <tedaneblake@gmail.com>
2021-01-04 17:26:58 -0500

    added loading screen

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,14 +13,15 @@ import { Backdrop } from '@material-ui/core';
 import { Button as MyButton } from './FormElements';
 import { useDispatch } from 'react-redux';
 import { deletePost, likePost } from '../actions/posts';
-
+import loadingImg from '../images/spin.svg';
 import { Card, CardImage, CardInfo, CardActions, DeleteModal } from './PostElements';
 
 const Post = ({ setCurrentId }) => {
 
-
+   const [isLoading, setIsLoading] = useState(true);
    const dispatch = useDispatch();
    const posts = useSelector((state) => state.posts);
+   setIsLoading(false);
 
    const [open, setOpen] = React.useState(false);
 
@@ -43,97 +44,111 @@ const Post = ({ setCurrentId }) => {
 
    return (
       <>
-         {posts.map((post) => (
-            <Card key={post._id}>
-               <div className="card-top">
-                  <span className="creator">{post.creator}</span>
-                  <span className="date">{moment(post.createdAt).fromNow()}</span>
-               </div>
-               {post.selectedFile === '' ? <CardImage src='https://dummyimage.com/250x350/181a1a/bfbfbf.png&text=no+image+found' noImage={true} /> :
-
-                  <CardImage src={post.selectedFile} />
-               }
-               <CardInfo>
-                  <span className="title">{post.title}</span>
-                  <span className="message">{post.message}</span>
-               </CardInfo>
-               <div className="tags">
-                  {post.tags.map((tag) => `#${tag} `)}
-               </div>
-               <CardActions>
-                  <Button onClick={() => dispatch(likePost(post._id))}>
-                     <FavoriteIcon />
-                     <span className='likes'>{post.likeCount}</span>
-                  </Button>
-
-                  <Button onClick={handleClickOpen} type="button">
-                     <DeleteIcon />
-                  </Button>
-                  <Modal
-                     open={open}
-                     onClose={handleClose}
-                     aria-labelledby="simple-modal-title"
-                     aria-describedby="simple-modal-description"
-                     closeAfterTransition
-                     className="modal"
-                     BackdropComponent={Backdrop}
-                     BackdropProps={{
-                        timeout: 500,
-                     }}
-                  >
-                     <Fade in={open}>
-                        <DeleteModal>
-                           <h3 id="title">
-                              "Are you sure you want to delete this memory?"
-                           </h3>
-                           <div className="del-actions">
-                              <MyButton
-                                 onClick={handleClose}
-                                 className="small-button"
-                              >
-                                 Cancel
-                              </MyButton>
-
-                              <MyButton
-                                 autoFocus
-                                 onClick={() => {
-                                    dispatch(deletePost(post._id));
-                                    handleClose();
-                                 }}
-                                 className="small-button"
-                                 primary={true}
-                              >
-                                 Yes
-                              </MyButton>
+         <div>
+            {isLoading ?
+               <div className="loading">
+                  <div className="loading-content">
+                     <h1>Loading...</h1>
+                     <img src={loadingImg} alt="loading" />
+                  </div>
+               </div> :
+               <>
+                  {
+                     posts.map((post) => (
+                        <Card key={post._id}>
+                           <div className="card-top">
+                              <span className="creator">{post.creator}</span>
+                              <span className="date">{moment(post.createdAt).fromNow()}</span>
                            </div>
-                        </DeleteModal>
-                     </Fade>
-                  </Modal>
+                           {post.selectedFile === '' ? <CardImage src='https://dummyimage.com/250x350/181a1a/bfbfbf.png&text=no+image+found' noImage={true} /> :
 
-                  <PopupState variant="popover" popupId="popup-menu">
-                     {(popupState) => (
-                        <>
-                           <Button {...bindTrigger(popupState)} className="small-button">
-                              <MoreHorizIcon />
-                           </Button>
-                           <Menu {...bindMenu(popupState)}>
-                              <MenuItem
-                                 onClick={() => {
-                                    setCurrentId(post._id); popupState.close();
-                                 }
-                                 }
+                              <CardImage src={post.selectedFile} />
+                           }
+                           <CardInfo>
+                              <span className="title">{post.title}</span>
+                              <span className="message">{post.message}</span>
+                           </CardInfo>
+                           <div className="tags">
+                              {post.tags.map((tag) => `#${tag} `)}
+                           </div>
+                           <CardActions>
+                              <Button onClick={() => dispatch(likePost(post._id))}>
+                                 <FavoriteIcon />
+                                 <span className='likes'>{post.likeCount}</span>
+                              </Button>
+
+                              <Button onClick={handleClickOpen} type="button">
+                                 <DeleteIcon />
+                              </Button>
+                              <Modal
+                                 open={open}
+                                 onClose={handleClose}
+                                 aria-labelledby="simple-modal-title"
+                                 aria-describedby="simple-modal-description"
+                                 closeAfterTransition
+                                 className="modal"
+                                 BackdropComponent={Backdrop}
+                                 BackdropProps={{
+                                    timeout: 500,
+                                 }}
                               >
-                                 Edit
+                                 <Fade in={open}>
+                                    <DeleteModal>
+                                       <h3 id="title">
+                                          "Are you sure you want to delete this memory?"
+                           </h3>
+                                       <div className="del-actions">
+                                          <MyButton
+                                             onClick={handleClose}
+                                             className="small-button"
+                                          >
+                                             Cancel
+                              </MyButton>
+
+                                          <MyButton
+                                             autoFocus
+                                             onClick={() => {
+                                                dispatch(deletePost(post._id));
+                                                handleClose();
+                                             }}
+                                             className="small-button"
+                                             primary={true}
+                                          >
+                                             Yes
+                              </MyButton>
+                                       </div>
+                                    </DeleteModal>
+                                 </Fade>
+                              </Modal>
+
+                              <PopupState variant="popover" popupId="popup-menu">
+                                 {(popupState) => (
+                                    <>
+                                       <Button {...bindTrigger(popupState)} className="small-button">
+                                          <MoreHorizIcon />
+                                       </Button>
+                                       <Menu {...bindMenu(popupState)}>
+                                          <MenuItem
+                                             onClick={() => {
+                                                setCurrentId(post._id); popupState.close();
+                                             }
+                                             }
+                                          >
+                                             Edit
                               </MenuItem>
-                              <MenuItem onClick={popupState.close}>Hide</MenuItem>
-                              <MenuItem onClick={popupState.close}>Save</MenuItem>
-                           </Menu>
-                        </>
-                     )}
-                  </PopupState>
-               </CardActions>
-            </Card>
-         ))}
+                                          <MenuItem onClick={popupState.close}>Hide</MenuItem>
+                                          <MenuItem onClick={popupState.close}>Save</MenuItem>
+                                       </Menu>
+                                    </>
+                                 )}
+                              </PopupState>
+                           </CardActions>
+                        </Card>
+                     ))
+                  }
+               </>
+            }
+         </div>
       </>
    )
 }
