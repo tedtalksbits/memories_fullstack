1ce514f616f9c334904a9670c93cbcd6600d482a
tedtalksbits <tedaneblake@gmail.com>
2021-01-04 17:36:52 -0500

    killed some bugs - imported useState

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
 import DeleteIcon from '@material-ui/icons/Delete';
